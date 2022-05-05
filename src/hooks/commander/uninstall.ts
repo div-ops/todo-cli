ebf4ae4b97ee82203d23c6fbf8a94b6124a153fd
seokho-jeong <seokho.jeong@toss.im>
2022-05-06 02:41:10 +0900

    fix: uninstall

--- src/hooks/commander/uninstall.ts
+++ src/hooks/commander/uninstall.ts
@@ -1,4 +1,4 @@
-import { unregisterAlias } from "../../utils";
+// import { unregisterAlias } from "../../utils";
 import { exec } from "../../utils/exec";
 import { useRouter } from "../router";
 
@@ -6,7 +6,7 @@ export function useUninstall() {
   const router = useRouter();
 
   return async () => {
-    const message = await unregisterAlias("todo");
+    // const message = await unregisterAlias("todo");
 
     console.log(
       await exec("yarn global remove @divops/todo-cli", {
@@ -14,6 +14,6 @@ export function useUninstall() {
       })
     );
 
-    return router.push("message", { query: { message } });
+    return router.push("message", { query: { message: "uninstalled" } });
   };
 }
